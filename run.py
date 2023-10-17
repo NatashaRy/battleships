edd47e0fe83d01e523dfbffd02106fc0fd60fe34
NatashaRy <natasha@briljantkommuikation.se>
2023-10-17 12:54:12 +0200

    feat: added game boards + print function.

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
+#Game boards
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [['_'] * 8 for x in range(8)]
 
+letters_to_numbers = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
 
-# Game boards
+def print_board(board):
+    """
+    Printing the game boards.
+    """
+    print ('  A B C D E F G H')
+
+    row_num = 1
+    for row in board:
+        print(f"{row_num}|{'|'.join(row)}|")
+        row_num += 1 
 
 #Create ships
 
@@ -11,7 +24,7 @@
 
 
 
-print("Welcome to Battleships!")
-print("Board size: , numbers of ships: \n")
-print("How to play: \n")
-name = input("What is your name: ")
+#print("Welcome to Battleships!")
+#print("Board size: , numbers of ships: \n")
+#print("How to play: \n")
+#name = input("What is your name: ")
