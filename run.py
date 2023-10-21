cca1ea34798978d0684f0104bb04a9e9b2e9e353
NatashaRy <natasha@briljantkommuikation.se>
2023-10-21 02:56:49 +0200

    fix: tried to fix logic for msg when ship is sunk

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import re
 HIDDEN_BOARD = [["_"] * 8 for _ in range(8)]
 GUESS_BOARD = [["_"] * 8 for _ in range(8)]
 PLAYERS_BOARD = [["_"] * 8 for _ in range(8)]
+player_sunk_count = 0
+computer_sunk_count = 0
 
 
 def reset_game():
@@ -13,8 +15,11 @@ def reset_game():
     HIDDEN_BOARD[:] = [["_"] * 8 for _ in range(8)]
     GUESS_BOARD[:] = [["_"] * 8 for _ in range(8)]
     PLAYERS_BOARD[:] = [["_"] * 8 for _ in range(8)]
+    player_sunk_count[:] = 0
+    computer_sunk_count[:] = 0
     for ship, details in SHIPS.items():
         details["sunk"] = False
+
 
 def print_board(board):
     """
@@ -29,10 +34,29 @@ def print_board(board):
 
 
 SHIPS = {
-    "Battlehip": {"size": 4, "char": "B", "position": None, "orientation": None, "sunk": False},
-    "Submarine": {"size": 3, "char": "S", "position": None, "orientation": None, "sunk": False},
-    "Patrol boat": {"size": 2, "char": "P", "position": None, "orientation": None, "sunk": False},
+    "Battlehip": {
+        "size": 4,
+        "char": "B",
+        "position": None,
+        "orientation": None,
+        "sunk": False,
+    },
+    "Submarine": {
+        "size": 3,
+        "char": "S",
+        "position": None,
+        "orientation": None,
+        "sunk": False,
+    },
+    "Patrol boat": {
+        "size": 2,
+        "char": "P",
+        "position": None,
+        "orientation": None,
+        "sunk": False,
+    },
 }
+
 
 def create_computer_ships(board):
     """
@@ -67,6 +91,7 @@ def create_computer_ships(board):
                         board[ship_row + i][ship_column] = ship_char
                     placed = True
 
+
 def place_players_ships(board):
     """
     Let player place their ships on board, by choosing starting position and orientation.
@@ -85,10 +110,6 @@ def place_players_ships(board):
             position = input(
                 f"Please enter starting position for {ship} (e.g., A1): "
             ).upper()
-            orientation = input(
-                "Enter orientation (H for horizontal, V for vertical): "
-            ).upper()
-
             if (
                 len(position) < 2
                 or position[0] not in "ABCDEFGH"
@@ -96,6 +117,10 @@ def place_players_ships(board):
             ):
                 print("Invalid position. Please use the format 'A1', 'C3' etc.")
                 continue
+
+            orientation = input(
+                "Enter orientation (H for horizontal, V for vertical): "
+            ).upper()
 
             column = ord(position[0]) - ord("A")
             row = int(position[1:]) - 1
@@ -167,6 +192,7 @@ def players_guess():
 
     return int(row) - 1, letters_to_numbers[column]
 
+
 def computer_guess(board):
     """
     Let the computer randomly guess the placement of the player's ships.
@@ -175,8 +201,8 @@ def computer_guess(board):
     column, row = random.randint(0, 7), random.randint(0, 7)
     while board[row][column] == "X" or board[row][column] == "-":
         row, column = random.randint(0, 7), random.randint(0, 7)
-
     return row, column
+
 
 def count_char_on_board(board, char):
     """
@@ -184,32 +210,25 @@ def count_char_on_board(board, char):
     """
     return sum(row.count(char) for row in board)
 
-
-def check_ship_sunk(board, is_player=True):
+def check_ship_sunk_player(board):
     """
-    Check if any ship has been sunk on the given board, both for the player and computer.
+    Check if the player have sunk any ships on the given board.
     Returns the name of the sunk ship or None if no ship was sunk.
     """
     for ship, details in SHIPS.items():
         char_count = count_char_on_board(board, details["char"])
-        if char_count == details["size"] and not details["sunk"]:
+        if char_count == 0 and not details["sunk"]:
             details["sunk"] = True
-            if is_player:
-                return ship
-            else:
-                return ship
-    return None
+            return ship
 
-def main():
+def players_turn():
     """
-    Run all game functions.
+    Players game logic.
     """
-    sunk_ship_for_round = None
-    place_players_ships(PLAYERS_BOARD)
+    global player_sunk_count
+    print_board(GUESS_BOARD)
 
-    create_computer_ships(HIDDEN_BOARD)
     while True:
-        print_board(GUESS_BOARD)
         row, column = players_guess()
         if GUESS_BOARD[row][column] == "X":
             print("You already guessed that, try again.\n")
@@ -220,35 +239,62 @@ def main():
                 if HIDDEN_BOARD[row][column] == details["char"]:
                     hit_ship = ship
                     break
-        if hit_ship:
-            GUESS_BOARD[row][column] = HIDDEN_BOARD[row][column]
-            print(f"You hit the {hit_ship}!\n")
-            sunk_ship_by_player = check_ship_sunk(GUESS_BOARD, is_player = True)
-            if sunk_ship_by_player:
-                print(f"You have sunk the computer's {sunk_ship_by_player}!\n")              
-        else:
-            print("Miss!\n")
-            GUESS_BOARD[row][column] = "X"
+            if hit_ship:
+                GUESS_BOARD[row][column] = HIDDEN_BOARD[row][column]
+                print(f"You hit the {hit_ship}!\n")
+            else:
+                print("Miss!\n")
+                GUESS_BOARD[row][column] = "X"
+            break
 
-        computer_row, computer_column = computer_guess(PLAYERS_BOARD)
-        hit_ship = None
-        for ship, details in SHIPS.items():
-            if PLAYERS_BOARD[computer_row][computer_column] == details["char"]:
-                hit_ship = ship
-                break
-        if hit_ship:
-            PLAYERS_BOARD[computer_row][computer_column] = "X"
-            print_board(PLAYERS_BOARD)
-            print(f"Computer hit your {hit_ship}!\n")
-            sunk_ship_by_computer = check_ship_sunk(PLAYERS_BOARD, is_player=False)
-            if sunk_ship_by_computer:
-                print(f"Computer has sunk your {sunk_ship_by_computer}!")                
-        else:
-            PLAYERS_BOARD[computer_row][computer_column] = "-"
-            print_board(PLAYERS_BOARD)
-            print("Computer missed!\n")
+    sunk_ship_by_player = check_ship_sunk_player(GUESS_BOARD)
+    if sunk_ship_by_player:
+        print(f"You have sunk the computer's {sunk_ship_by_player}!\n")
+        player_sunk_count += 1
+    return player_sunk_count
 
-    restart = input("Do you want to play again? (y/n): \n").upper()
+
+def computers_turn():
+    """
+    Computers game logic.
+    """
+    global computer_sunk_count
+    computer_row, computer_column = computer_guess(PLAYERS_BOARD)
+    hit_ship = None
+    for ship, details in SHIPS.items():
+        if PLAYERS_BOARD[computer_row][computer_column] == details["char"]:
+            hit_ship = ship
+            break
+    if hit_ship:
+        PLAYERS_BOARD[computer_row][computer_column] = "X"
+        print_board(PLAYERS_BOARD)
+        print(f"Computer hit your {hit_ship}!\n")
+    else:
+        PLAYERS_BOARD[computer_row][computer_column] = "-"
+        print_board(PLAYERS_BOARD)
+        print("Computer missed!\n")
+
+
+
+
+def main():
+    """
+    Run all game functions.
+    """
+    place_players_ships(PLAYERS_BOARD)
+    create_computer_ships(HIDDEN_BOARD)
+    while True:
+        players_turn()
+        if player_sunk_count == len(SHIPS):
+            print(f"Congratulations {name}, you won!")
+            break
+
+        computers_turn()
+        if computer_sunk_count == len(SHIPS):
+            print(f"Sorry {name}, the computer one!")
+            break
+
+    restart = input("Do you want to play again? (y/n): ").upper()
     if restart == "Y":
         reset_game()
         main()
